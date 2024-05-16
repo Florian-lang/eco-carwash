1d0eae06d301c8324525874df05cdfe0c56e26b9
Alex <alex.lemmaire@gmail.com>
2024-05-16 20:02:22 +0200

    add all stuff to permit launch app (not work with gps)

--- lib/WashStation/washStation_page.dart
+++ lib/WashStation/washStation_page.dart
@@ -1,6 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class WashStationPage extends StatelessWidget{
   const WashStationPage({super.key, required this.name, required this.address, required this.latitude, required this.longitude});
@@ -8,7 +8,20 @@ class WashStationPage extends StatelessWidget{
   final String address;
   final int latitude;
   final int longitude;
-  // final double price;
+  // final List<dynamic> prices;
+
+  void _launchGPS() async {
+    // const String url = "sms:";
+    // const String url = "https://flutter.dev/";
+    final String url = "geo:47.6,-122.3";
+    final Uri uri = Uri.parse(url);
+
+    if(await canLaunchUrl(uri)){
+      await launchUrl(uri);
+    }else{
+      throw "Impossible de lancer $url";
+    }
+  }
 
   @override
   Widget build(BuildContext context){
@@ -21,19 +34,29 @@ class WashStationPage extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Text(name, style: const TextStyle(fontSize: 40)),
-            Text(address),
-            const SizedBox(height: 60),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(height: 20,),
-                Text('La latitude : $latitude', style: const TextStyle(fontSize: 20)),
-                Text('La longitude : $longitude', style: const TextStyle(fontSize: 20)),
-                const SizedBox(height: 20,)
-              ],
+            GestureDetector(
+              onTap: (){
+                _launchGPS();
+                },
+              child: Container(
+                child: Column(
+                  children: [
+                    Text(address),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(height: 20,),
+                        Text('La latitude : $latitude', style: const TextStyle(fontSize: 20)),
+                        Text('La longitude : $longitude', style: const TextStyle(fontSize: 20)),
+                        const SizedBox(height: 20,)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
             const SizedBox(height: 60),
-            // Text("Prix :  $price", style: const TextStyle(fontSize: 30)),
+            // Text("Prix :  $prices", style: const TextStyle(fontSize: 30)),
             const SizedBox(height: 60),
 
           ],
