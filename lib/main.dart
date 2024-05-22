13784a425f672817afcc95ae9ea349def1e72144
Alex <alex.lemmaire@gmail.com>
2024-05-22 18:12:22 +0200

    modification of userpage to have appreciation

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'Entity/WashStation.dart';
+import 'model/wash_station.dart';
 import 'loginPage.dart';
 import 'WashStation/washStationPage.dart';
 
